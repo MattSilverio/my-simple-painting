5d23ccb7c0b682582600e3d654ae287624f8920e
Matheus Silverio <matheusphillipo@gmail.com>
2023-05-17 12:52:33 -0300

    feat: function to handle keydown events

---
+++ src/utils/actions.js
@@ -0,0 +1,45 @@
+// @ts-ignore
+export function trapFocus(node) {
+	const previous = document.activeElement;
+
+	function focusable() {
+		return Array.from(
+			node.querySelectorAll(
+				'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+			)
+		);
+	}
+
+	// @ts-ignore
+	function handleKeydown(event) {
+		if (event.key !== 'Tab') return;
+
+		const current = document.activeElement;
+
+		const elements = focusable();
+		const first = elements.at(0);
+		const last = elements.at(-1);
+
+		if (event.shiftKey && current === first) {
+			last.focus();
+			event.preventDefault();
+		}
+
+		if (!event.shiftKey && current === last) {
+			first.focus();
+			event.preventDefault();
+		}
+	}
+
+	focusable()[0]?.focus();
+
+	node.addEventListener('keydown', handleKeydown);
+
+	return {
+		destroy() {
+			node.removeEventListener('keydown', handleKeydown);
+			// @ts-ignore
+			previous?.focus();
+		}
+	};
+}
